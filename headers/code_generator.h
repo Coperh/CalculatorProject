d236b29817e9657a0d55b6f8e7cd7bcdc5be2fdf
Conor Holden <cholden48@gmail.com>
2019-10-21 19:11:54 +0100

    Fixed the main branch

--- headers/code_generator.h
+++
@@ -1,6 +0,0 @@
-#ifndef CODE_GENERATOR_H
-#define CODE_GENERATOR_H
-
-void code_generator();
-
-#endif // CODE_GENERATOR_H
--- headers/infix2postfix.h
+++
@@ -1,7 +0,0 @@
-#ifndef INFIX2POSTFIX_H
-#define INFIX2POSTFIX_H
-
-void infix2postfix();
-
-#endif // !INFIX2POSTFIX_H
-
