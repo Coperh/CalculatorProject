6969c0b46184b711f0d13b098cc1470b63b187df
Conor Holden <cholden48@gmail.com>
2019-10-21 13:26:49 +0100

    sdfsdddfsd

--- headers/tokenizer.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-
-#ifndef TOKENIZER_H
-#define TOKENIZER_H
-int tokenizer();
-
-#endif // TOKENIZER_H
--- headers/virtual_machine.h
+++
@@ -1,7 +0,0 @@
-#ifndef VIRTUAL_MACHINE_h
-#define VIRTUAL_MACHINE_h
-
-void virtual_machine();
-
-
-#endif // VIRTUAL_MACHINE_h
