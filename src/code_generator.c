d15b59184dc6bc038cbc6c41000ba8557edf3e34
William Yang <31744376+WillyBlueSax@users.noreply.github.com>
2019-11-26 20:27:33 +0000

    Update code_generator.c

--- src/code_generator.c
+++ src/code_generator.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,35 +13,33 @@ int NumberDetector(char *number) {
     int i = 0;
 
   while (number[i] != '\0') {
-    // if decimal count
-    if (number[i] == '.') 
+    if (number[i] == '.') //if decimal count
       decimalCount++;
     else if( !isdigit(number[i]))
         return -1;
     i++;
     }
-    
-    if (decimalCount >= 1) //if there are 1 or more dots, it's a float=
+    if (decimalCount >= 1) //if there are 1 or more dots, its a float
         return 0;
     return 1;
 }
 
-
+//Code Generator main
 int code_generator( int argc, char** argv ){
 
-    const char *delimiter_characters = " ";
-    const char *filename = "Postfix.txt";
+    const char *delimiter_characters = " "; //if there is a blank space 
+    const char *filename = "Postfix.txt"; //input file name
 
     FILE *input_file, *output_file;
 
-	input_file = fopen(filename, "r");
-	output_file = fopen("Instructions.txt", "w");
+    input_file = fopen(filename, "r"); //reads input from "Postfix.txt" file
+    output_file = fopen("Instructions.txt", "w"); //write output to "Instructions.txt" file
 
-    char buffer[ BUFFER_SIZE ];
+    char buffer[ BUFFER_SIZE ]; // Buffer size is define above 
     char *last_token;
 
     if(input_file == NULL){
-        fprintf( stderr, "Unable to open file %s\n", filename );
+        fprintf( stderr, "Unable to open file %s\n", filename ); // If the file can't be opened, it prints out an error statement
         return -1;
     }
 	else{
@@ -56,35 +51,33 @@ int code_generator( int argc, char** argv ){
             last_token = strtok( buffer, delimiter_characters );
             while( last_token != NULL ){
 
-
                 switch(last_token[0]){
                     case '+':
-                        fprintf(output_file,"ADD\n");
+                        fprintf(output_file,"ADD\n"); // Prints out the instruction for the operator '+'
                         break;
 
                     case '-':
-                        fprintf(output_file, "SUB\n");
+                        fprintf(output_file, "SUB\n"); // Prints out the instructions for the operator '-'
                         break;
 
                     case '*':
-                        fprintf(output_file,"MUL\n");
+                        fprintf(output_file,"MUL\n"); // Prints out the instructions for the operator '*'
                         break;
 
                     case '/':
-                        fprintf(output_file, "DIV\n");
+                        fprintf(output_file, "DIV\n"); // Prints out the instructions for the operator '/'
                         break;
 
                     default:
-                        if (last_token[0] == '\n'){
-                            break;
+                        if (last_token[0] == '\n'){ 
+                            break; // If there is a new line break out of switch 
                         }
                         if(NumberDetector(last_token) == 0)
-                            fprintf(output_file, "LOADFLOAT %s\n", last_token);
-                        
+                            fprintf(output_file, "LOADFLOAT %s\n", last_token); // Print out the instructions for a float token
                         else if(NumberDetector(last_token) == 1)
-                            fprintf(output_file, "LOADINT %s\n", last_token);
+                            fprintf(output_file, "LOADINT %s\n", last_token); // Prints out the instructions for an int token
                         else{
-                            printf("Invlaid input\n");
+                            printf("Invlaid input\n"); // If there is an invalid input, return -1
                             return -1;
                             
                         }
@@ -92,12 +85,13 @@ int code_generator( int argc, char** argv ){
                 last_token = strtok(NULL, delimiter_characters );
             }
             if(ferror(input_file) ){
-                perror( "The following error occurred" );
+                perror( "The following error occurred" ); 
             }
 
         }
     }
-		fclose( input_file );
-		fclose( output_file );
+    fclose( input_file ); // closes input file
+    fclose( output_file ); // closes output file
+	
     return 0;
 }
