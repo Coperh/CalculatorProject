884e05298c01196957265905cde56bbc7d822ca9
Conor Holden <cholden48@gmail.com>
2019-11-26 15:11:03 +0000

    Infix test now runs without error Files are closed before they are read, loop runs for correct amount of times

--- src/infix2postfixTest.c
+++ src/infix2postfixTest.c
@@ -6,22 +6,26 @@
 //requirement C.1: Produce correct order of operations
 //requirement C.2: Handle parentheses
 TESTS{
-
+    ok(1 == 1, "test");
+    
 	char buffer[100];
 	FILE *fp;
 	FILE *outfp;
 	char *input[] = {"1 + 3", "10 + 11 + 12","2 * ( 10 + 1.25 )"};
 	char *output[] = {"1 3 + ", "10 11 + 12 + ", "2 10 1.25 + * "};
-	fp = fopen("Tokens.txt", "w");
 
-	for (int i = 0; i < sizeof(input);i++){
-		fputs(input[i], fp);
-		infix2postfix();
-		outfp = fopen("Postfix.txt", "r");
-		fgets(buffer, 100, outfp);
-		is( buffer, output[i], "<%s> == <%s>", buffer,output[i]);
+    
+	for (int i = 0; i < sizeof(input)/sizeof(input[0]);i++){
+        int i = 0;
+        fp = fopen("Tokens.txt", "w");
+        fputs(input[i], fp);
+        fclose(fp);
+        infix2postfix();
+        outfp = fopen("Postfix.txt", "r");
+        fgets(buffer, 100, outfp);
+        is( buffer, output[i], "<%s> == <%s>", buffer,output[i]);
+        fclose(outfp);
 	}
-	fclose(fp);
-	fclose(outfp);
+    
 }
 
